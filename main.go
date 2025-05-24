383ddebce60d24c32ade07f77c3768919a255c77
孔德丞 <2480155857@qq.com>
2025-05-24 14:16:32 +0800

    refactor(server): 重构服务器代码并添加链接处理函数
    - 将服务器相关代码从 main.go 移动到 server.go 文件
    - 新增 handle 函数用于处理客户端连接
    - 在 Start 函数中使用 goroutine 并发处理多个连接
    - 简化了 main 函数，仅保留服务器创建和启动逻辑

--- main.go
+++ main.go
@@ -1,46 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"net"
-)
-
-type Server struct {
-	Ip   string
-	Port int
+func main() {
+    server := NewServer("127.0.0.1", 8080)
+    server.Start()
 }
-
-// 创建一个服务器
-func NewServer(ip string, port int) *Server {
-	server := &Server{
-		Ip:   ip,
-		Port: port,
-	}
-	return server
-}
-
-//  启动服务器
-func (this *Server) Start() {
-	//socket listen
-	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
-	if err != nil {
-		fmt.Println("net.Listen err:", err)
-		return
-	}
-	defer listener.Close()
-	for {
-		//accept
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("listener.Accept err:", err)
-			return
-		}
-		//handle
-		//close
-
-	}
-		
-
-	}
-
-	
--- server.go
+++ server.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+}
+
+// 创建一个服务器
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+	return server
+}
+
+func (this *Server) handle (conn net.Conn) { 
+	fmt.Println("链接建立成功")
+}
+//  启动服务器
+func (this *Server) Start() {
+	//socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+	defer listener.Close()
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener.Accept err:", err)
+			return
+		}
+		//handle
+		go this.handle(conn)
+		//close
+
+	}
+		
+
+	}
+
+	
