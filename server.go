1f32cb0f01eafe24a3ed5a10319b331a27de4c35
孔德丞 <2480155857@qq.com>
2025-06-07 21:47:51 +0800

    feat(server): 实现服务器基本功能
    - 添加用户上线逻辑，将新用户加入在线用户列表
    - 实现广播功能，向所有在线用户发送消息
    - 新增用户结构体和相关方法，用于处理用户连接和消息发送
    - 优化服务器启动流程，增加消息监听goroutine

--- server.go
+++ server.go
@@ -3,26 +3,68 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+	//  在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.Mutex
+	//  广播消息
+	msg chan string
 }
 
 // 创建一个服务器
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		mapLock:   sync.Mutex{},
+		//添加一个广播频道
+		msg:       make(chan string),
 	}
 	return server
 }
 
-func (this *Server) handle (conn net.Conn) { 
-	fmt.Println("链接建立成功")
+//  处理用户连接
+func (this *Server) handle(conn net.Conn) {
+	//fmt.Println("链接建立成功")
+	user := NewUser(conn)
+	//用户上线加入map
+	this.mapLock.Lock()
+	//添加到map中
+	this.OnlineMap[user.Name] = user
+	//解开锁
+	this.mapLock.Unlock()
+
+	//向所有用户广播
+	this.BroadCast(user, "已上线")
+	//	阻塞
+	select {}
+
 }
-//  启动服务器
+//  广播
+func (this *Server) BroadCast(user *User, msg string) { 
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.msg <- sendMsg
+}
+
+//监听msg 
+func (this *Server) ListenMsg() { 
+	for {
+		msg := <-this.msg
+		//  遍历所有用户
+		this.mapLock.Lock()
+		for _, user := range this.OnlineMap {
+			user.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+// 启动服务器
 func (this *Server) Start() {
 	//socket listen
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
@@ -31,6 +73,9 @@ func (this *Server) Start() {
 		return
 	}
 	defer listener.Close()
+
+	//启动监听
+	go this.ListenMsg()
 	for {
 		//accept
 		conn, err := listener.Accept()
@@ -41,10 +86,6 @@ func (this *Server) Start() {
 		//handle
 		go this.handle(conn)
 		//close
-
-	}
-		
-
 	}
 
-	
+}
--- user.go
+++ user.go
@@ -1,6 +1,44 @@
 package main
 
-type User struct { 
+import (
+	"net"
+)
+
+//  User 用户结构体
+type User struct {
 	Name string
-	
+	Addr string
+	C    chan string
+	con  net.Conn
 }
+
+// NewUser 创建并初始化一个新的用户对象。
+// 该函数接收一个网络连接对象作为参数，用于后续与用户进行通信。
+// 返回值是一个指向User结构的指针，表示新创建的用户实例。
+func NewUser(con net.Conn) *User {
+    // 获取客户端的地址信息，将其用作用户的名称和地址。
+    userAddr := con.RemoteAddr().String()
+    
+    // 初始化一个User结构体实例。
+    // 用户名称和地址都设置为客户端的地址信息。
+    // 创建一个字符串类型的通道，用于向用户发送消息。
+    // 将网络连接对象赋值给用户实例，以便后续进行读写操作。
+    user := &User{
+        Name: userAddr,
+        Addr: userAddr,
+        C:    make(chan string),
+        con:  con,
+    }
+    // 启动一个goroutine来监听用户发送的消息。
+	go user.ListenMessage()
+    // 返回初始化后的用户实例。
+    return user
+}
+//  ListenMessage 监听用户发送的消息。
+func (this *User) ListenMessage() { 
+	for {
+		msg := <-this.C
+		this.con.Write([]byte(msg + "\n"))
+	}
+}
+
